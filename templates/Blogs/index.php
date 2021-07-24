6678536c99da0dfff61659d65d3b9e65c475548c
Marcelo Rocha <contato@omarcelo.com.br>
2021-07-23 21:35:26 -0300

    Remove invalid title

--- templates/Blogs/index.php
+++ templates/Blogs/index.php
@@ -10,30 +10,13 @@ $this->Html->css(['styles.css'], ['block' => true,])
 <!-- Responsive navbar-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="#!">Start Bootstrap</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Blog</a></li>
-            </ul>
-        </div>
+        <a class="navbar-brand" href="#!"><?= __('The Awesome Blog')?></a>
     </div>
 </nav>
 <!-- Page content-->
 <div class="container mt-5">
     <div class="row">
         <div class="col-lg-8">
-            <!-- Post content-->
-            <article>
-                <!-- Post header-->
-                <header class="mb-4">
-                    <!-- Post title-->
-                    <h1 class="fw-bolder mb-1"><?= __('The Awesome Blog')?></h1>
-                </header>
-            </article>
             <?php if (!$blogs):?>
             <div class="alert alert-info">
                 <strong><?= __('Not found blog entries')?></strong>
